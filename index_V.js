0a12714d27e7a1f29d088fe63ea34d89a675c295
vicruijim <vicruijim@alum.us.es>
2023-03-03 23:38:07 +0100

    funcion Victor

--- index_V.js
+++ index_V.js
@@ -1,4 +1,4 @@
-
+/*
 function log(msg){
     console.log(msg);
 
@@ -16,11 +16,11 @@ o.l("log del objeto")
 
 
 numbers  = [1,2,3,4];
-/*
+
 for (var i = 0; i<numbers.length; i++){
     log(numbers[i])
 }
-*/
+
 
 
 numbers.forEach(function log(msg){
@@ -35,13 +35,59 @@ numbers.forEach((msg)=>{
     console.log("v2:"+msg)
 });
 
-number.filter((n)=>{
+numbers.filter((n)=>{
     return n>2;
 }).map((n)=>{
     return n+1;
 }).reduce((a,n)=>{
     return n+a;
 }).forEach(log);
+*/
 
 
+//----------------------------------------
+// L05
 
+
+var  cool = require("cool-ascii-faces");
+console.log(cool());
+
+var express = require("express");
+var app = express();
+var port = 12345
+app.get("/faces", (req,res)=> {
+    res.send(cool());
+    console.log("New Request");
+});
+
+app.listen(port,()=>{
+    console.log(`Server ready in port ${port}`);
+});
+
+
+//------------------------------------------
+//L06 
+
+var contacts = [{
+                    name: "people",
+                    phone: 12345                
+                },
+                {
+                    name: "pablo",
+                    phone: 6789
+                }
+];
+
+const BASE_API_URL = "/api/v1";
+
+app.get(BASE_API_URL+"/contacts", (req,res)=> {
+    res.json(contacts);
+    console.log("New GET Request to /contacts");
+});
+
+app.post(BASE_API_URL+"/contacts", (req,res)=> {
+    var newContact = request.body;
+    console.log(`new contact <${newContact}>`)
+    console.log("New POST  to /contacts");
+    res.sendStatus(201)
+});
